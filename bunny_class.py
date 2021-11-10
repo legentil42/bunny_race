d2053cf634010da52ce3783b0567da2a30ef95c3
Francois Wieckowiak <legentil42@gmail.com>
2021-11-10 12:52:39 +0000

    race and death

--- bunny_class.py
+++ bunny_class.py
@@ -3,7 +3,13 @@ import random
 from math import *
 import sys
 from pygame.locals import *
-TAUX_MUTATION_GLOBAL = 0.5 #pourcentage
+TAUX_MUTATION_GLOBAL = 1 #pourcentage
+NB_LAPIN = 6
+TAUX_VAINQUEURS = 0.5
+pygame.init()
+jump_sound = pygame.mixer.Sound("jump.wav")
+finish_sound = pygame.mixer.Sound("finish.wav")
+death_sound = pygame.mixer.Sound("death.wav")
 class Bunny:
     width,height = 200,200
     instances = []
@@ -27,6 +33,16 @@ class Bunny:
     sprites_d.append(pygame.image.load('sprites/bunny/bunny_d/frame_5.png'))
     sprites_d.append(pygame.image.load('sprites/bunny/bunny_d/frame_6.png'))
     sprites_d.append(pygame.image.load('sprites/bunny/bunny_d/frame_7.png'))
+
+    sprites_dead_d = []
+    sprites_dead_d.append(pygame.image.load('sprites/bunny/dead_d/frame_0.png'))
+    sprites_dead_d.append(pygame.image.load('sprites/bunny/dead_d/frame_1.png'))
+    sprites_dead_d.append(pygame.image.load('sprites/bunny/dead_d/frame_2.png'))
+    sprites_dead_d.append(pygame.image.load('sprites/bunny/dead_d/frame_3.png'))
+    sprites_dead_d.append(pygame.image.load('sprites/bunny/dead_d/frame_4.png'))
+    sprites_dead_d.append(pygame.image.load('sprites/bunny/dead_d/frame_5.png'))
+    sprites_dead_d.append(pygame.image.load('sprites/bunny/dead_d/frame_6.png'))
+    sprites_dead_d.append(pygame.image.load('sprites/bunny/dead_d/frame_7.png'))
 
     def __init__(self,parent1=None,parent2=None,desired_y = None):
         self.__class__.instances.append(self)
@@ -69,6 +85,7 @@ class Bunny:
         self.rect = self.image.get_rect()
         self.rect.topleft = [self.x,self.y]
         self.jump_animation = False
+        self.dead = False
 
     def new_x_et_y(self):
         self.new_angle =0# 2*pi*random.random()
@@ -84,45 +101,53 @@ class Bunny:
         print("new waypoint")
         self.jump_animation = True
         self.new_x_et_y()
-        while self.new_x >= 800 or self.new_x <= 0 or self.new_y >= 600 or self.new_y <= 0:
+        while self.new_x <= 0 or self.new_y >= 600 or self.new_y <= 0:
             self.new_x_et_y()
-
+        pygame.mixer.Sound.play(jump_sound)
         self.is_wandering = True
 
     def update(self,dt):
-        if self.is_wandering:
-            if sqrt((self.x-self.new_x)**2 + (self.y-self.new_y)**2)>20:
-                x_parcouru = cos(self.new_angle)*self.speed*dt
-                y_parcouru = sin(self.new_angle)*self.speed*dt
-                self.x = self.x +  x_parcouru
-                self.y = self.y + y_parcouru
-                #print(sqrt((self.x-self.new_x)**2 + (self.y-self.new_y)**2))
-            else:
-                self.is_wandering = False
-                self.jump_animation = False
-        
-        else:
-            if random.random() < self.wander_proba:
-                self.waypoint()
-
-        if self.jump_animation == True:
-            if self.current_dir == "droite":
-                self.current_sprite += self.__class__.jump_animation_speed
-                if self.current_sprite >= len(self.__class__.sprites_d):
-                    self.current_sprite = 0
-                self.image = self.__class__.sprites_d[int(self.current_sprite)]
-            elif self.current_dir == "gauche":
-                self.current_sprite += self.__class__.jump_animation_speed
-                if self.current_sprite >= len(self.__class__.sprites_g):
-                    self.current_sprite = 0
-                self.image = self.__class__.sprites_g[int(self.current_sprite)]
-        else:
-            self.current_sprite = 0
-            if self.current_dir == "droite":
-                self.image = self.__class__.sprites_d[self.current_sprite]
+        if self.dead == False:
+            if self.is_wandering:
+                if sqrt((self.x-self.new_x)**2 + (self.y-self.new_y)**2)>20:
+                    x_parcouru = cos(self.new_angle)*self.speed*dt
+                    y_parcouru = sin(self.new_angle)*self.speed*dt
+                    self.x = self.x +  x_parcouru
+                    self.y = self.y + y_parcouru
+                    #print(sqrt((self.x-self.new_x)**2 + (self.y-self.new_y)**2))
+                else:
+                    self.is_wandering = False
+                    self.jump_animation = False
             
-            elif self.current_dir == "gauche":
-                self.image = self.__class__.sprites_g[self.current_sprite]
+            else:
+                if random.random() < self.wander_proba:
+                    self.waypoint()
+
+            if self.jump_animation == True:
+                if self.current_dir == "droite":
+                    self.current_sprite += self.__class__.jump_animation_speed
+                    if self.current_sprite >= len(self.__class__.sprites_d):
+                        self.current_sprite = 0
+                    self.image = self.__class__.sprites_d[int(self.current_sprite)]
+                elif self.current_dir == "gauche":
+                    self.current_sprite += self.__class__.jump_animation_speed
+                    if self.current_sprite >= len(self.__class__.sprites_g):
+                        self.current_sprite = 0
+                    self.image = self.__class__.sprites_g[int(self.current_sprite)]
+            else:
+                self.current_sprite = 0
+                if self.current_dir == "droite":
+                    self.image = self.__class__.sprites_d[self.current_sprite]
+                
+                elif self.current_dir == "gauche":
+                    self.image = self.__class__.sprites_g[self.current_sprite]
+        
+        if self.dead == True:
+            self.jump_animation = False
+            self.image = self.__class__.sprites_dead_d[int(self.current_sprite)]
+            if self.current_sprite < len(self.__class__.sprites_dead_d)-1:
+                self.current_sprite += 0.2
+            
 
     def draw_bunny(self,screen,font):
         txt_a_afficher = str(round(self.speed,1))+","+ str(round(self.wander_dist_coef,2))+","+str(round(self.wander_proba,2))
--- main.py
+++ main.py
@@ -11,25 +11,6 @@ import numpy as np
 BLUE = [0,0,255]
 
 
-
-Bunny.instances = []
-
-
-
- 
-pygame.init()
-pygame.font.init()
-font = pygame.font.SysFont('Comic Sans MS', 30)
-
-fps = 60
-dt = 1/fps
-fpsClock = pygame.time.Clock()
- 
-width, height = 800, 600
-screen = pygame.display.set_mode((width, height))
-img_fond = pygame.image.load('fond.png')
-
-
 def generate_x_bunnies(nb_bun):
     L_des_Y = np.linspace(1,450,nb_bun)
     for i in range(nb_bun):
@@ -38,31 +19,82 @@ def generate_x_bunnies(nb_bun):
         else:
             Bunny(P1,P1,desired_y=L_des_Y[nb_bun-i-1])
 
-generate_x_bunnies(5)
-# Game loop.
-while True:
 
-  
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.MOUSEBUTTONUP:
+
+
+
+def generation():
+
+    Bunny.instances = []
+    generate_x_bunnies(NB_LAPIN)
+    bunnies_order = []
+    time_elapsed = 0
+    sim_finished = False
+
+
+    fps = 60
+    dt = 1/fps
+    fpsClock = pygame.time.Clock()
+    
+    width, height = 800, 600
+    screen = pygame.display.set_mode((width, height))
+    img_fond = pygame.image.load('fond.png')
+
+    pygame.init()
+    pygame.font.init()
+    font = pygame.font.SysFont('Comic Sans MS', 30)
+
+    # Game loop
+    while len(bunnies_order)<NB_LAPIN:
+
+    #input
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONUP:
+                for bunny in Bunny.instances:
+                    del bunny
+                Bunny.instances = []
+                generate_x_bunnies(NB_LAPIN)
+                bunnies_order = []
+                time_elapsed = 0
+                sim_finished = False
+    
+    # Update.
+        if len(bunnies_order)>=TAUX_VAINQUEURS*NB_LAPIN and sim_finished == False:
+            sim_finished = True
             for bunny in Bunny.instances:
-                del bunny
-            Bunny.instances = []
-            generate_x_bunnies(5)
-  
-  # Update.
-  
-  # Draw.
-    screen.blit(img_fond, (0,0))
-    for bunny in Bunny.instances: 
-        bunny.update(dt)
-        bunny.draw_bunny(screen,font)
+                bunny.current_sprite = 0
+                bunny.dead = True
+            pygame.mixer.Sound.play(death_sound)
+                
+    # Draw.
+        screen.blit(img_fond, (0,0))
+        for bunny in Bunny.instances:
+            if sim_finished == False:
+
+                if bunny not in [i[0] for i in bunnies_order]:
+                    bunny.update(dt)
+                    bunny.draw_bunny(screen,font)
+                    if bunny.x > 780:
+                        pygame.mixer.Sound.play(finish_sound)
+                        bunnies_order.append([bunny,time_elapsed])
+                        Bunny.instances.pop(Bunny.instances.index(bunny))
+                        del bunny
+            
+            else:
+                bunny.update(dt)
+                bunny.draw_bunny(screen,font)    
 
 
-    pygame.display.flip()
-    dt = fpsClock.tick(fps)/1000
+        pygame.display.flip()
+        dt = fpsClock.tick(fps)/1000
+        time_elapsed += dt
+    pygame.quit()
+    return bunnies_order
 
+
+
+generation()
 # %%
